02e8cd06f562b8e00dba0c014d5e3e5c8fba922e
alamshafil <36016500+alamshafil@users.noreply.github.com>
2020-08-12 16:14:25 -0400

    Add files via upload

---
+++ ipa2deb/Form1.Designer.cs
@@ -0,0 +1,198 @@
+﻿namespace ipa2deb
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.panel1 = new System.Windows.Forms.Panel();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.button1 = new System.Windows.Forms.Button();
+            this.openFileDialog1 = new System.Windows.Forms.OpenFileDialog();
+            this.button2 = new System.Windows.Forms.Button();
+            this.textBox2 = new System.Windows.Forms.TextBox();
+            this.button3 = new System.Windows.Forms.Button();
+            this.folderBrowserDialog1 = new System.Windows.Forms.FolderBrowserDialog();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Segoe UI", 20.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.Location = new System.Drawing.Point(12, 9);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(272, 37);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "Welcome to ipa2deb!";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("Segoe UI", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label2.Location = new System.Drawing.Point(15, 46);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(375, 21);
+            this.label2.TabIndex = 1;
+            this.label2.Text = "Select an IPA and output file, then press on \"Convert\"";
+            // 
+            // panel1
+            // 
+            this.panel1.BackColor = System.Drawing.Color.Black;
+            this.panel1.Location = new System.Drawing.Point(-3, 71);
+            this.panel1.Name = "panel1";
+            this.panel1.Size = new System.Drawing.Size(660, 10);
+            this.panel1.TabIndex = 2;
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Font = new System.Drawing.Font("Segoe UI", 20.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label3.Location = new System.Drawing.Point(12, 101);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(163, 37);
+            this.label3.TabIndex = 3;
+            this.label3.Text = "IPA Location";
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Font = new System.Drawing.Font("Segoe UI", 20.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label4.Location = new System.Drawing.Point(12, 217);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(210, 37);
+            this.label4.TabIndex = 4;
+            this.label4.Text = "Output Location";
+            // 
+            // textBox1
+            // 
+            this.textBox1.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.textBox1.Enabled = false;
+            this.textBox1.Font = new System.Drawing.Font("Segoe UI", 9F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.textBox1.Location = new System.Drawing.Point(19, 142);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(371, 23);
+            this.textBox1.TabIndex = 5;
+            // 
+            // button1
+            // 
+            this.button1.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.button1.Font = new System.Drawing.Font("Segoe UI", 8.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.button1.Location = new System.Drawing.Point(397, 141);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(75, 25);
+            this.button1.TabIndex = 6;
+            this.button1.TabStop = false;
+            this.button1.Text = "Select";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // openFileDialog1
+            // 
+            this.openFileDialog1.FileName = "openFileDialog1";
+            // 
+            // button2
+            // 
+            this.button2.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.button2.Font = new System.Drawing.Font("Segoe UI", 8.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.button2.Location = new System.Drawing.Point(397, 256);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(75, 25);
+            this.button2.TabIndex = 8;
+            this.button2.TabStop = false;
+            this.button2.Text = "Select";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // textBox2
+            // 
+            this.textBox2.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.textBox2.Enabled = false;
+            this.textBox2.Font = new System.Drawing.Font("Segoe UI", 9F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.textBox2.Location = new System.Drawing.Point(19, 257);
+            this.textBox2.Name = "textBox2";
+            this.textBox2.Size = new System.Drawing.Size(371, 23);
+            this.textBox2.TabIndex = 7;
+            // 
+            // button3
+            // 
+            this.button3.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.button3.Font = new System.Drawing.Font("Segoe UI", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.button3.Location = new System.Drawing.Point(506, 310);
+            this.button3.Name = "button3";
+            this.button3.Size = new System.Drawing.Size(121, 36);
+            this.button3.TabIndex = 9;
+            this.button3.Text = "Convert";
+            this.button3.UseVisualStyleBackColor = true;
+            this.button3.Click += new System.EventHandler(this.button3_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.White;
+            this.ClientSize = new System.Drawing.Size(654, 357);
+            this.Controls.Add(this.button3);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.textBox2);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.textBox1);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.panel1);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedSingle;
+            this.MaximizeBox = false;
+            this.Name = "Form1";
+            this.ShowIcon = false;
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "ipa2deb";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Panel panel1;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.OpenFileDialog openFileDialog1;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.TextBox textBox2;
+        private System.Windows.Forms.Button button3;
+        private System.Windows.Forms.FolderBrowserDialog folderBrowserDialog1;
+    }
+}
+
---
+++ ipa2deb/Form1.cs
@@ -0,0 +1,125 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Diagnostics;
+using System.Drawing;
+using System.IO;
+using System.IO.Compression;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace ipa2deb
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            openFileDialog1.InitialDirectory = "c:\\";
+            openFileDialog1.Filter = "Apple IPA File (*.ipa)|*.ipa";
+            openFileDialog1.FilterIndex = 2;
+            openFileDialog1.RestoreDirectory = true;
+
+            if (openFileDialog1.ShowDialog() == DialogResult.OK)
+            {
+                textBox1.Text = openFileDialog1.FileName; 
+            }
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            if (folderBrowserDialog1.ShowDialog() == DialogResult.OK)
+            {
+                textBox2.Text = folderBrowserDialog1.SelectedPath;
+            }
+        }
+
+        private void button3_Click(object sender, EventArgs e)
+        {
+            if(string.IsNullOrEmpty(textBox1.Text))
+            {
+                MessageBox.Show("You didn't select an IPA file.", "ipa2deb", MessageBoxButtons.OK, MessageBoxIcon.Exclamation);
+
+            } else if(string.IsNullOrEmpty(textBox2.Text))
+            {
+                MessageBox.Show("You didn't select an output folder.", "ipa2deb", MessageBoxButtons.OK, MessageBoxIcon.Exclamation);
+
+            } else
+            {
+                if (Directory.Exists(Directory.GetCurrentDirectory() + @".\temp"))
+                {
+                    if (MessageBox.Show("The temp folder already exists, press OK and try again.", "ipa2deb", MessageBoxButtons.OK, MessageBoxIcon.Information) == DialogResult.OK)
+                    {
+                        Directory.Delete(Directory.GetCurrentDirectory() + @".\temp", true);
+                    }
+                }
+                else
+                {
+
+                    Cursor.Current = Cursors.AppStarting;
+                    string name = textBox1.Text.Split(Path.DirectorySeparatorChar).Last().Replace(".ipa", "");
+
+                    Directory.CreateDirectory(Directory.GetCurrentDirectory() + @".\temp");
+                    Directory.CreateDirectory(Directory.GetCurrentDirectory() + @".\temp\zip");
+                    ZipFile.ExtractToDirectory(textBox1.Text, Directory.GetCurrentDirectory() + @".\temp\zip");
+                    Directory.CreateDirectory(Directory.GetCurrentDirectory() + @".\temp\app");
+
+                    string dir = Directory.GetDirectories(Directory.GetCurrentDirectory() + @"\temp\zip\Payload\")[0];
+                    dir = dir.Split(Path.DirectorySeparatorChar).Last();
+
+                    Directory.Move(Directory.GetCurrentDirectory() + @".\temp\zip\Payload\" + dir, Directory.GetCurrentDirectory() + @".\temp\app\" + dir);
+
+                    Directory.Delete(Directory.GetCurrentDirectory() + @".\temp\zip", true);
+
+                    Directory.CreateDirectory(Directory.GetCurrentDirectory() + @".\temp\deb\");
+                    Directory.CreateDirectory(Directory.GetCurrentDirectory() + @".\temp\deb\DEBIAN");
+                    Directory.CreateDirectory(Directory.GetCurrentDirectory() + @".\temp\deb\Applications");
+
+                    Directory.Move(Directory.GetCurrentDirectory() + @".\temp\app\" + dir, Directory.GetCurrentDirectory() + @".\temp\deb\Applications\" + dir);
+                    Directory.Delete(Directory.GetCurrentDirectory() + @".\temp\app", true);
+
+                    using (StreamWriter sw = new StreamWriter(Directory.GetCurrentDirectory() + @".\temp\deb\DEBIAN\control"))
+                    {
+                        sw.WriteLine("Package: com.ipa2deb." + name);
+                        sw.WriteLine("Name:" + name);
+                        sw.WriteLine("Version: 1.0");
+                        sw.WriteLine("Description: This app was converted with ipa2deb.");
+                        sw.WriteLine("Maintainer: " + Environment.UserName);
+                        sw.WriteLine("Author: ipa2deb");
+                        sw.Close();
+                    }
+
+                    string exePath = Directory.GetCurrentDirectory() + @"\temp\deb\wpkg.exe";
+                    File.WriteAllBytes(exePath, ipa2deb.Properties.Resources.wpkg);
+
+                    var processInfo = new ProcessStartInfo(exePath, $"-b {Path.Combine(Directory.GetCurrentDirectory() + @"\temp\deb\")}");
+                    processInfo.WorkingDirectory = textBox2.Text;
+                    processInfo.CreateNoWindow = true;
+                    processInfo.UseShellExecute = false;
+
+                    var process = Process.Start(processInfo);
+
+                    process.WaitForExit();
+
+                    Console.WriteLine("ExitCode: {0}", process.ExitCode);
+                    process.Close();
+
+                    File.Copy(Directory.GetCurrentDirectory() + @"\temp\deb\ipa2deb.deb", textBox2.Text + @"\" + name + ".deb" );
+
+                    Directory.Delete(@".\temp", true);
+                    Cursor.Current = Cursors.Default;
+
+                    MessageBox.Show("Finished converting ipa to deb!", "ipa2deb", MessageBoxButtons.OK, MessageBoxIcon.Information);
+                }
+            }
+        }
+
+    }
+}
